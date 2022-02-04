8624092c5930d85c685a7c9397901c10479ade87
Greg Slepak <contact@taoeffect.com>
2022-02-03 19:35:09 -0800

    fixed failing test

--- index.test.js
+++ index.test.js
@@ -1,9 +1,7 @@
 /* eslint-env mocha */
 
 import should from 'should'
-import sinon from 'sinon'
 import sbp from '@sbp/sbp'
-import 'should-sinon'
 import './dist/module.mjs'
 
 describe('[SBP] okTurtles.eventQueue domain', () => {
