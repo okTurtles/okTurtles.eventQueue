699f8abaf0fca68535407002f43795e9e4a65a26
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2023-10-18 21:34:15 +0200

    Global introspection and function arguments

--- dist/umd/index.cjs
+++ dist/umd/index.cjs
@@ -4,15 +4,6 @@
 // Use this to ensure asynchronous SBP invocations get invoked serially,
 // one after another, waiting for the previous invocation to finish completely.
 // =======================
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -28,49 +19,78 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     const sbp_1 = __importDefault(require("@sbp/sbp"));
+    const isEventQueueSbpEvent = (e) => {
+        return Object.prototype.hasOwnProperty.call(e, 'sbpInvocation');
+    };
     exports.default = (0, sbp_1.default)('sbp/selectors/register', {
         'okTurtles.eventQueue/_init': function () {
             this.eventQueues = Object.create(null);
         },
         'okTurtles.eventQueue/isWaiting': function (name) {
             var _a;
-            return !!((_a = this.eventQueues[name]) === null || _a === void 0 ? void 0 : _a.events.length);
+            return !!((_a = this.eventQueues[name]) === null || _a === void 0 ? void 0 : _a.length);
         },
         'okTurtles.eventQueue/queuedInvocations': function (name) {
             var _a, _b;
-            return (_b = (_a = this.eventQueues[name]) === null || _a === void 0 ? void 0 : _a.events.map((event) => event.sbpInvocation)) !== null && _b !== void 0 ? _b : [];
+            if (name == null) {
+                return Object.fromEntries(Object.entries(this.eventQueues).map(([name, events]) => [name, events.map((event) => {
+                        if (isEventQueueSbpEvent(event)) {
+                            return event.sbpInvocation;
+                        }
+                        else {
+                            return event.fn;
+                        }
+                    })]));
+            }
+            return (_b = (_a = this.eventQueues[name]) === null || _a === void 0 ? void 0 : _a.map((event) => {
+                if (isEventQueueSbpEvent(event)) {
+                    return event.sbpInvocation;
+                }
+                else {
+                    return event.fn;
+                }
+            })) !== null && _b !== void 0 ? _b : [];
         },
-        'okTurtles.eventQueue/queueEvent': function (name, sbpInvocation) {
-            return __awaiter(this, void 0, void 0, function* () {
-                if (!Object.prototype.hasOwnProperty.call(this.eventQueues, name)) {
-                    this.eventQueues[name] = { events: [] };
+        'okTurtles.eventQueue/queueEvent': async function (name, invocation) {
+            if (!Object.prototype.hasOwnProperty.call(this.eventQueues, name)) {
+                this.eventQueues[name] = [];
+            }
+            const events = this.eventQueues[name];
+            let accept;
+            const promise = new Promise((resolve) => { accept = resolve; });
+            const thisEvent = typeof invocation === 'function'
+                ? {
+                    fn: invocation,
+                    promise
                 }
-                const events = this.eventQueues[name].events;
-                let accept;
-                const thisEvent = {
-                    sbpInvocation,
-                    promise: new Promise((resolve) => { accept = resolve; })
+                : {
+                    sbpInvocation: invocation,
+                    promise
                 };
-                events.push(thisEvent);
-                while (events.length > 0) {
-                    const event = events[0];
-                    if (event === thisEvent) {
-                        try {
-                            return yield (0, sbp_1.default)(...event.sbpInvocation);
+            events.push(thisEvent);
+            while (events.length > 0) {
+                const event = events[0];
+                if (event === thisEvent) {
+                    try {
+                        if (typeof invocation === 'function') {
+                            return await invocation();
                         }
-                        finally {
-                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                            // @ts-ignore
-                            accept();
-                            events.shift();
+                        else {
+                            return await (0, sbp_1.default)(...invocation);
                         }
                     }
-                    else {
-                        // wait for invocation to finish
-                        yield event.promise;
+                    finally {
+                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                        // @ts-ignore
+                        accept();
+                        events.shift();
                     }
                 }
-            });
+                else {
+                    // wait for invocation to finish
+                    await event.promise;
+                }
+            }
         }
     });
 });
