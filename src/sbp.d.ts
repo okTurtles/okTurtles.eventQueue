148084f2a5f11cfd41899eaaca6d63699185a553
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2025-03-10 20:38:10 +0000

    Add types

--- src/sbp.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@sbp/sbp' {
-  export type SbpInvocation = [string, ...unknown[]]
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const _default: (...args: SbpInvocation) => any
-  export default _default
-}
